ca5862f5def70fee1a03b6abda2afb08002eae9c
Labush <82900721+Labush@users.noreply.github.com>
2022-06-17 22:36:41 +0200

    Update vuejs.js

--- js/vuejs.js
+++ js/vuejs.js
@@ -1,46 +1,65 @@
-/******https://github.com/Labush/JS******/
-new Vue({
+const app = new Vue({
     el: '#app',
-    data: function () {
-        return {
-            items: [
-                {
-                    titre: 'Menu 1',
-                    description: 'Lorem ipsum dolor sit ipsum vouptarum amet consectetur adipisicing elit.',
-                    prix: '3.50',
-                    photoURL: 'img/img_menu/dej.jpg'
-                },
-                {
-                    titre: 'Menu 2',
-                    description: 'Lorem ipsum dolor sit ipsum vouptarum amet consectetur adipisicing elit.',
-                    prix: '7.50',
-                    photoURL: 'img/img_menu/dej2.jpg'
-                },
-                {
-                    titre: 'Menu 3',
-                    description: 'Lorem ipsum dolor sit ipsum vouptarum amet consectetur adipisicing elit.',
-                    prix: '13',
-                    photoURL: 'img/img_menu/menu.jpg'
-                },
-                {
-                    titre: 'Menu 4',
-                    description: 'Lorem ipsum dolor sit ipsum vouptarum amet consectetur adipisicing elit.',
-                    prix: '4.50',
-                    photoURL: 'img/img_menu/menu2.jpg'
-                },
-                {
-                    titre: 'Menu 5',
-                    description: 'Lorem ipsum dolor sit ipsum vouptarum amet consectetur adipisicing elit.',
-                    prix: '7.90',
-                    photoURL: 'img/img_menu/soir.jpg'
-                },
-                {
-                    titre: 'Menu 6',
-                    description: 'Lorem ipsum dolor sit ipsum vouptarum amet consectetur adipisicing elit.',
-                    prix: '12',
-                    photoURL: 'img/img_menu/soir2.jpg'
-                }
-            ]
+    data: {
+        Petitdejeuners: {
+            1: {
+                name: 'Menu 1',
+                description: 'Lorem ipsum dolor sit ipsum vouptarum amet consectetur adipisicing elit.',
+                prix: 3.5,
+                photoURL: '../images/petitdej1.PNG'
+            },
+            2: {
+                name: 'Menu 2',
+                description: 'Lorem ipsum dolor sit ipsum vouptarum amet consectetur adipisicing elit.',
+                prix: 7.5,
+                photoURL: '../images/petitdej2.PNG'
+            },
+            3: {
+                name: 'Menu 3',
+                description: 'Lorem ipsum dolor sit ipsum vouptarum amet consectetur adipisicing elit.',
+                prix: 13,
+                photoURL: '../images/petitdej3.PNG'
+            }
+        },
+        Dejeuners: {
+            1: {
+                name: 'Menu 1',
+                description: 'Lorem ipsum dolor sit ipsum vouptarum amet consectetur adipisicing elit.',
+                prix: 3.5,
+                photoURL: '../images/midi1.PNG'
+            },
+            2: {
+                name: 'Menu 2',
+                description: 'Lorem ipsum dolor sit ipsum vouptarum amet consectetur adipisicing elit.',
+                prix: 7.5,
+                photoURL: '../images/midi2.PNG'
+            },
+            3: {
+                name: 'Menu 3',
+                description: 'Lorem ipsum dolor sit ipsum vouptarum amet consectetur adipisicing elit.',
+                prix: 13,
+                photoURL: '../images/midi3.PNG'
+            }
+        },
+        Diners: {
+            1: {
+                name: 'Diner 1',
+                description: 'Lorem ipsum dolor sit ipsum vouptarum amet consectetur adipisicing elit.',
+                prix: 12,
+                photoURL: '../images/soir1.PNG'
+            },
+            2: {
+                name: 'Diner 2',
+                description: 'Lorem ipsum dolor sit ipsum vouptarum amet consectetur adipisicing elit.',
+                prix: 14.5,
+                photoURL: '../images/soir2.PNG'
+            },
+            3: {
+                name: 'Diner 3',
+                description: 'Lorem ipsum dolor sit ipsum vouptarum amet consectetur adipisicing elit.',
+                prix: 17,
+                photoURL: '../images/soir3.PNG'
+            }
         }
     }
 })
